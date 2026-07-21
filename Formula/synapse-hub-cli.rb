61a839b9e4933be320434f755b8563f8c3b15ca9
Rodrigo Vizcarra <rvizcarra@ithub.cl>
2026-07-20 21:44:12 -0400

    fix SHA256 after tar.gz normalization

--- Formula/synapse-hub-cli.rb
+++ Formula/synapse-hub-cli.rb
@@ -6,16 +6,16 @@ class SynapseHubCli < Formula
 
   if Hardware::CPU.intel? && OS.mac?
     url "https://raw.githubusercontent.com/ithubcode/synapse-hub-releases/main/v1.0.0/synapse-hub-cli_darwin_amd64.tar.gz"
-    sha256 "b4c91e70a618060e81c1c60a14d1e07974a0f94d765789de86ce778411ccefae"
+    sha256 "eed9b36e1327d353f260e28401f72d1d0389fb3710931ff85d478c0550bcc76a"
   elsif Hardware::CPU.arm? && OS.mac?
     url "https://raw.githubusercontent.com/ithubcode/synapse-hub-releases/main/v1.0.0/synapse-hub-cli_darwin_arm64.tar.gz"
-    sha256 "e8b1cf8e0475b76735871b3629a9b8b1f0f2725bd3a31baa01959e9399fada8a"
+    sha256 "95178d4e7a603dc2e0c3589f8968754a83e4c9d5c4a4612330a22f118397bd7d"
   elsif Hardware::CPU.intel? && OS.linux?
     url "https://raw.githubusercontent.com/ithubcode/synapse-hub-releases/main/v1.0.0/synapse-hub-cli_linux_amd64.tar.gz"
-    sha256 "cc880995da2473d59e2d230273b326101a7ef3583f722d07926fe0b985285a67"
+    sha256 "439b476ca1eb87e6af6cc06d0757bea084e83b1ea8e665f0c7d3b452a4fc18e5"
   elsif Hardware::CPU.arm? && OS.linux?
     url "https://raw.githubusercontent.com/ithubcode/synapse-hub-releases/main/v1.0.0/synapse-hub-cli_linux_arm64.tar.gz"
-    sha256 "abfae5c4910467357e8cacc7dc775ca73c8a33671f9d8099856dd5ba6b777985"
+    sha256 "f1ca882ceed37b8afb130125700b71ee3821b3b78bf89b5b9b3258a15c6e1c82"
   end
 
   def install
